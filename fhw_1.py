fc76ca23ac4c2b40ad325236709031ba975d3a3f
Evgeniy <Slipknot07@yandex.ru>
2024-09-26 14:14:57 +0800

    fhw_1.py
    Доп. задание по 1 модулю.

--- fhw_1.py
+++ fhw_1.py
@@ -1,4 +1,6 @@
-students = {'Евгений', 'Анастасия', 'Игорь', 'Светлана', 'Татьяна'}
-grades = [[5, 4, 5, 5, 4], [3, 2, 2, 2, 3], [3, 4, 5, 5, 2], [4, 4, 4, 4, 3], [5, 5, 3, 4, 5]]
-a = 5  # Количество предметов
-print(grades)
+students = {'Евгений', 'Анастасия', 'Игорь', 'Светлана', 'Татьяна', 'Виктория', 'Александр', 'Мария', 'Оксана', 'Федор'}
+grades = [[5,4,5,5,4], [3,5,2,3], [3,4,5,2], [4,4,4,4,3], [5,5,3,4,5], [1,2,3,4,5], [3,3,3,3,3], [4,5,5,5], [3,4,4,3,5], [5,3,3,3,3]]       
+grades_avrsum = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4]), sum(grades[5])/len(grades[5]), sum(grades[6])/len(grades[6]), sum(grades[7])/len(grades[7]), sum(grades[8])/len(grades[8]), sum(grades[9])/len(grades[9])]
+students_sort = sorted(students)
+dnevnik = dict(zip(students_sort, grades_avrsum))
+print(dnevnik)
